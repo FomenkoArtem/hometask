96e8648b10d4853f66d6f4a4b0d9410e69a122ee
Artem Fomenko <fomenko.artem2005@mail.ru>
2023-10-08 15:17:45 +0300

    Добавлена возможность вывести 'Hello World' на трех разных языках(Английский,Русский,Французский) по выбору пользователя

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  Print('Hello world!');
+  var l := ReadString('Choose your language(English,Русский,Français):');
+  
+  if l = 'English' then
+    Print('Hello World!')
+  else if l = 'Русский' then
+    Print('Привет Мир!')
+  else if l = 'Français' then
+    Print('bonjour le monde!');
 end.
